e23cbe6e4e27f533a551198eb922ca94f81e3fa8
vishalu439 <vishalumrao6@gmail.com>
2023-01-31 13:48:29 +0530

    weather

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import styles from './App.module.css';
 import {useState,useEffect} from "react"
 import Weather from './weather';
-let key="fb9a1444ba0719551b596b9e04aa8a7a"
+
 
 
 
@@ -9,50 +9,11 @@ let key="fb9a1444ba0719551b596b9e04aa8a7a"
 
 function App() {
   
-  const[data,newData]=useState([])
-  const[loading,setLoading]=useState("true")
-  const[city,updateCity]=useState("")
-  let handleSubmit=(e)=>{
-    e.preventDefault()
-    
-    fetchData()
-    
-    
-    
-    }
-    
 
-  const fetchData=async ()=>{
-    try {
-      const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-    const data=await response.json()
-    newData(data)
-    setLoading(false)
-    //console.log(data)
-    } catch (error) {
-      throw error;
-      setLoading(false)
-      console.log('error')
-    }
     
-    
-    
-    }
-    useEffect(()=>{
-
-    fetchData()
-
-    },[])
   return (
     <div className="App">
-    <form >
-      <input name="city" id="city" type="text" placeholder="City" value={city} onChange={(e)=>updateCity(e.target.value)}/>
-        <button type="submit" onClick={handleSubmit}>Search</button>
-        </form>
-      <div className={styles.container}>
-       
-        </div>
-        <Weather data={data} loading={loading}/>
+    <Weather/>
        
 
       </div>
--- src/weather.jsx
+++ src/weather.jsx
@@ -1,48 +1,43 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import styles from './weather.module.css';
+let key="fb9a1444ba0719551b596b9e04aa8a7a"
+const Weather = () => {
+  const [weather, setWeather] = useState({});
+  const [city, setCity] = useState("London");
 
-let Weather=({data,loading})=>{
-    console.log(data)
-    
-    if(loading)
-    {
-      return(
-      <h1>loading...</h1>
+  useEffect(() => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setWeather(data);
+      });
+      console.log(weather)
+  }, [city]);
 
-
-
-      )
-
-    }
-
-else{
-
-return(
-<>
-
-<h1>dtata</h1>
-
-
-
-
-
-
-
-
-
-</>
-
-
-
-
-
-
-
-
-)
-
-
-}
-
-
-}
+  return (
+    <div>
+      <form>
+        <label className={styles.input} htmlfor="city">Enter City</label>
+        <input
+         name="city"
+          className={styles.input}
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+      </form>
+      {weather.main && (
+        <div className={styles.container}>
+          <p>Temperature: {Math.floor(weather.main.temp-273.5)}°C</p>
+          <p>Feels Like {Math.floor(weather.main.feels_like-273.5)}°C</p>
+          <p>Humidity: {weather.main.humidity}%</p>
+          <p>Description: {weather.weather[0].description}</p>
+        </div>
+      )}
+      
+    </div>
+  );
+};
 export default Weather;
